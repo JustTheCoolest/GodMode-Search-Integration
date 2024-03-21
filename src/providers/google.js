3138a90721e40f2b5d126d4b69673bc01ae296e7
Andhavarapu Balu <74148176+JustTheCoolest@users.noreply.github.com>
2024-03-21 22:43:53 +0530

    cleared handleCss of google, bug got less intense

--- src/providers/google.js
+++ src/providers/google.js
@@ -31,28 +31,6 @@ class Google extends Provider {
 	}
 
 	static handleCss() {
-		this.getWebview().addEventListener('dom-ready', () => {
-			// hide message below text input, sidebar, suggestions on new chat
-			setTimeout(() => {
-				this.getWebview().insertCSS(`
-        /* hide the claude avatar in response */
-        .p-1.w-9.h-9.shrink-0 {
-          display: none;
-        }
-        /* reduce claude prompt margins */
-        .mx-4.md\:mx-12.mb-2.md\:mb-4.mt-2.w-auto {
-          margin: 0 !important;
-        }
-
-        `);
-			}, 1000);
-			setTimeout(() => {
-				this.getWebview().executeJavaScript(`{
-				// hide welcome back title
-				document.querySelector('h2').style.display = 'none';
-				}`);
-			}, 1000);
-		});
 	}
 
 	static handleDarkMode(isDarkMode) {
