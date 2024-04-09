d407a69f8051cf1028462de996a6a178efa80e00
Andhavarapu Balu <74148176+JustTheCoolest@users.noreply.github.com>
2024-04-09 22:28:52 +0530

    activated Provider.getUserAgent for Claude2 and Google

--- src/providers/google.js
+++ src/providers/google.js
@@ -52,7 +52,7 @@ class Google extends Provider {
 	}
 
 	static getUserAgent() {
-		'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36';
+		return 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36';
 	}
 
 	static isEnabled() {
